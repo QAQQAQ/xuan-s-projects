9cada9533ec57d79b16c8085ad368f1b6d246c29
ZhaoXuan <zhaoxuan379@pku.edu.cn>
2016-05-17 11:17:23 +0800

    2

--- wechatwall2/index.js
+++ wechatwall2/index.js
@@ -3,32 +3,44 @@
  */
 //http://123.206.71.158:8060   millie
 var PORT=8060;
+var token="millie";
+
 var http=require('http');
 var qs=require('qs');
-
-var TOKEN='millie';
 
 function checkSignature(params,token){
     var key=[token,params.timestamp,params.nonce].sort().join('');
     var sha1=require('crypto').createHash('sha1');
     sha1.update(key);
-    //System.out.println("params"+sha1.digest('hex'));
     return sha1.digest('hex')==params.signature;
 }
 
-var server=http.createServer(function(req,res){
-    var query=require('url').parse(req.url).query;
+var server=http.createServer(function(request,response){
+    console.log("test token url"+require('url').parse(request.url));
+    var query=require('url').parse(request.url).query;
     var params=qs.parse(query);
 
-    console.log(params);
-    console.log("token-->",TOKEN);
-
-    if(checkSignature(params,TOKEN)){
-        res.end(params.echostr);
-    }else{
+    if(!checkSignature(params,TOKEN)){
         response.end('signature fail');
+        return;
     }
+
+    if(request.method=='GET'){
+        response.end(params.echostr);
+    }else{
+        var postData="";
+        request.addListener("data",function(postchunk){
+            postdata +=postchunk;
+        });
+
+        //获取post数据
+        request.add("end",function(){
+            console.log(postdata);
+            response.end('success');
+        });
+    }
+
 });
 
 server.listen(PORT);
-console.log("Server running at port"+PORT+".");
+console.log("Server is running at port:"+PORT+".");
